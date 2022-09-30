1aed94f9cdcc4c21ce7dacf9592ed1f9396a1c12
cojored <66964845+cojored@users.noreply.github.com>
2022-09-30 18:17:45 -0400

    Fix Intents

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,12 @@ import CMDHandler from "./Handlers/CommandHandler.js";
 import {
   Client,
   IntentsBitField,
-  Partials,
-  GatewayIntentBits,
 } from "discord.js";
 import config from "../config.json" assert { type: "json" };
 
 export const client = new Client({
   intents: [
     IntentsBitField.Flags.GuildIntegrations,
-    IntentsBitField.Flags.GuildMessages,
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-  partials: [Partials.Message],
 });
 const CommandHandler = new CMDHandler();
 
